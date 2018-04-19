269c0db78e20e53b816cc38b4da44313add394a2
Jonathan Ayala <yojona@msn.com>
2018-04-18 22:07:40 -0700

    Remove colorPalette import, 'colorPalette' is defined but never used  no-unused-vars

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './index.css';
 import { sizes, light, dark, mdInactive } from './config/mappings'
-import { colorPalette } from './palette';
 
 class MaterialIcon extends React.Component {
     render() {
-        const { icon, size, invert, inactive, color, style } = this.props; 
+        const { icon, size, invert, inactive, color } = this.props; 
 
         const sizeMapped = sizes[size] || parseInt(size) || sizes['small'];
         const defaultColor = (invert && 'invert') ? light : dark;
         const inactiveColor = (inactive && 'inactive') ? mdInactive : '';
-        const propStyle = style || {}
 
-        const styleOverride = Object.assign(propStyle, {color: color ? color : '', fontSize: sizeMapped});
+        const styleOverride = {color: color ? color : '', fontSize: sizeMapped};
 
         return (
             <i className={`material-icons ${sizeMapped} ${defaultColor} ${inactiveColor}`} style={styleOverride} >{icon}</i>
