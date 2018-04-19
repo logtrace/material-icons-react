4e119c31700f0f1c2e5337779866b7e868ce4e7f
Jonathan Ayala <yojona@msn.com>
2018-04-18 21:54:17 -0700

    Add style prop support

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import { colorPalette } from './palette';
 
 class MaterialIcon extends React.Component {
     render() {
-        const { icon, size, invert, inactive, color } = this.props; 
+        const { icon, size, invert, inactive, color, style } = this.props; 
 
         const sizeMapped = sizes[size] || parseInt(size) || sizes['small'];
         const defaultColor = (invert && 'invert') ? light : dark;
         const inactiveColor = (inactive && 'inactive') ? mdInactive : '';
+        const propStyle = style || {}
 
-        const styleOverride = {color: color ? color : '', fontSize: sizeMapped};
+        const styleOverride = Object.assign(propStyle, {color: color ? color : '', fontSize: sizeMapped});
 
         return (
             <i className={`material-icons ${sizeMapped} ${defaultColor} ${inactiveColor}`} style={styleOverride} >{icon}</i>
