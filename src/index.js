6d02e0e073ddbeff10945e4fe9b02233ba332938
ajayns <ajay.ns@outlook.com>
2018-04-22 21:00:35 +0530

    Add prop-types

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './index.css';
-import { sizes, light, dark, mdInactive } from './config/mappings'
+import { sizes, light, dark, mdInactive } from './config/mappings';
 
 const MaterialIcon = ({ icon, size, invert, inactive, color, style }) => {
     const sizeMapped = sizes[size] || parseInt(size) || sizes['small'];
@@ -13,5 +15,13 @@ const MaterialIcon = ({ icon, size, invert, inactive, color, style }) => {
         <i className={`material-icons ${sizeMapped} ${defaultColor} ${inactiveColor}`} style={styleOverride} >{icon}</i>
     )
 }
+
+MaterialIcon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  size: PropTypes.any,
+  invert: PropTypes.boolean,
+  inactive: PropTypes.boolean
+};
+
 export default MaterialIcon;
 export * from './palette';
